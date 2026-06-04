9de8398708ad2d760c28c73b22a90f0a8b38dee5
atarm <atarm95184@gmail.com>
2026-06-04 23:54:50 +0800

    完成新增與刪除警告

--- todos/views.py
+++ todos/views.py
@@ -5,7 +5,7 @@ from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
-    todos = Todo.objects.all()
+    todos = Todo.objects.all().order_by("-created", "-important")
     print(todos)
 
     return render(request, "todos/list.html", {"todos": todos})
@@ -24,4 +24,13 @@ def todo_delete(request, id):
 
 
 def todo_create(request):
+
+    if request.method == "POST":
+        print(request.POST)
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("新增成功")
+            return redirect("todo-list")  # 儲存完回首頁
+
     return render(request, "todos/create.html", {"form": TodoForm()})
